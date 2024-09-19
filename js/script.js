59ce3b94da3034a3d56bca7d253a3a565624d934
Supriadi998 <adiarifah910@gmail.com>
2024-09-19 23:41:26 +0800

    memperbaharui section product

--- js/script.js
+++ js/script.js
@@ -16,9 +16,19 @@ document.querySelector('#search-button').onclick = (e) => {
   e.preventDefault();
 };
 
+// toggle class active untuk shopping cart
+
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
 // klik diluar elemen
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
+
 
 document.addEventListener('click', function(e) {
   if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -27,5 +37,41 @@ document.addEventListener('click', function(e) {
   if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove('active');
   }
+  if(!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
 });
 
+
+
+// Modal Box
+
+const itemDetailModal = document.querySelector('#item-detail-modal');
+
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+
+
+
+// klik tombol Close
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// klik di luar modal
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = 'none';
+  }
+};
