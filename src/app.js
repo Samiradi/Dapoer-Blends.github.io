0828b0abf99b532017b1d8d5f22ef9b129fb734f
Supriadi998 <adiarifah910@gmail.com>
2024-10-02 21:21:07 +0800

    menambhakan harga miniman

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('product', () => ({
+    Alpine.data('products', () => ({
         items: [
-            { id: 1, name: 'Kopi Hitam', img: 'kopi-hitam.jpg', price: 7000},
-            { id: 2, name: 'Matcha Green Tea', img: 'product2.jpg', price: 7000},
-            { id: 3, name: 'Kopi Hitam', img: '1.img', price: 7000},
-            { id: 4, name: 'Kopi Hitam', img: '1.img', price: 7000},
-            { id: 5, name: 'Kopi Hitam', img: '1.img', price: 7000},
+            { id: 1, name: 'Kopi Hitam', img: 'kopi-hitam-cr.jpg', price: 7000 },
+            { id: 2, name: 'Matcha Greentea Prafe', img: 'matcha-cr.jpg', price: 17000 },
+            { id: 4, name: 'Milkshake oreo blends Coklat dan Vanila', img: '1.img', price: 17000 },
+            { id: 5, name: 'Cappucino Cincau Blends', img: '1.img', price: 17000 },
+            { id: , name: 'Es Teh Manis', img: '1.img', price: 7000 },
+            { id: , name: 'Es Jeruk Segar', img: '1.img', price: 10000 },
+            { id: , name: 'Milo Blends', img: '1.img', price: 17000 },
+            { id: , name: 'Caramel Machiato', img: '1.img', price: 20000 },
+            { id: , name: 'Coklat Original', img: '1.img', price: 15000 },
+            { id: , name: 'Teh Tarik Blends', img: '1.img', price: 17000 },
+            { id: , name: 'Hot Cappucino', img: '1.img', price: 10000 },
+            { id: , name: 'Thai Tea', img: '1.img', price: 15000 },
+            { id: , name: 'Lemon Tea', img: '1.img', price: 17000 },
+            { id: , name: 'Red Velvet', img: '1.img', price: 12000 },
+            { id: , name: 'Pop Ice Blends', img: '1.img', price: 12000 },
+            { id: , name: 'Soda Susu Ceria', img: '1.img', price: 17000 },
+            { id: , name: 'Nutrisari Salju Blends', img: '1.img', price: 15000 },
+            { id: , name: 'Josua', img: '1.img', price: 10000 },
 
         ],
 
