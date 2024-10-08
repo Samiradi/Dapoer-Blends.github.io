6f7f6c6fded5505f15e3205cea30d1662988c82f
Supriadi998 <adiarifah910@gmail.com>
2024-10-08 16:43:51 +0800

    tambah

--- src/app.js
+++ src/app.js
@@ -28,7 +28,58 @@ document.addEventListener('alpine:init', () => {
         total: 0,
         quantity: 0,
         add(newItem) {
-            console.log(newItem);
+            // check apakah ada barang yang sama di cart
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+
+
+            // jika belum ada / cart masih kosong
+            if(!cartItem) {
+                this.items.push({...newItem, quantity: 1, total: newItem.price});
+                this.quantity++;
+                this.total += newItem.price;
+
+            } else {
+                // jka barang sudah ada, cek apakah barang beda atau sama dengan barang yang ada di cart
+                this.items = this.items.map((item) => {
+                    // jika barang berbeda
+                    if(item.id !== newItem.id) {
+                        return item;
+                    } else {
+                        // jika barang sudah ada, tambah quantity dan totalnya
+                        item.quantity++;
+                        item.total = item.price * item.quantity;
+                        this.quantity++;
+                        this.total += item.price;
+                        return item;
+                    }
+                });
+            }
+        },
+        remove(id) {
+            // ambil item yang mau di remove
+            const cartItem = this.items.find((item) => item.id === id);
+
+            // jka barang lebih dari 1
+            if(cartItem.quantity > 1) {
+                // Telusuri 1 1
+                this.items = this.items.map((item) => {
+                    // jika bukan barang yang di klik
+                    if(item.id !== id) {
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                })
+            } else if (cartItem.quantity === 1) {
+                // jika barang nya sisa 1
+                this.items = this.items.filter((item) => item.id !== id);
+                this.quantity--;
+                this.total -= cartItem.price;
+            }
         },
     });
 });
